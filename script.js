954fa2fff8aa63f6f4000ccc6faf813f0c4c6332
Jeremy Dassonville <jeremydassonville07@gmail.com>
2021-04-09 16:49:56 +0200

    Mis en place bootstrap page d'accueil

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Fonction qui récupère les données de l'API */
 
 function getData () {
-  return fetch('http://localhost:3000/api/cameras')
+  return fetch('https://ab-p5-api.herokuapp.com/api/cameras')
     .then(function (response) {
       return response.json()
   })
@@ -21,23 +21,32 @@ function displayData(myData) {
 
        /* Ajout de la structure HTML  */
 
+    const productColumn = document.createElement('div')
     const productContenant = document.createElement('div')
     const productImage = document.createElement('img')
+    const productBody = document.createElement('div')
     const productName = document.createElement('h3')
     const productPrice = document.createElement('p')
 
         /* Ajout des éléments sur la page */
 
-    displayProduct.appendChild(productContenant)
+    displayProduct.appendChild(productColumn)
+    productColumn.appendChild(productContenant)
     productContenant.appendChild(productImage)
-    productContenant.appendChild(productName)
-    productContenant.appendChild(productPrice)
+    productContenant.appendChild(productBody)
+    productBody.appendChild(productName)
+    productBody.appendChild(productPrice)
 
         /* Ajout des valeurs */
 
+    productColumn.setAttribute("class", "col-12 col-lg-4")
+    productContenant.setAttribute("class", "card")    
     productImage.setAttribute("src", myData[i].imageUrl)
-    productContenant.setAttribute("class", "cart__container")
+    productImage.setAttribute("class", "card-img-top")
+    productBody.setAttribute("class", "card-body")
+    productName.setAttribute("class", "card-title")
     productName.innerHTML += myData[i].name
+    productPrice.setAttribute("class", "card-text")
     productPrice.innerHTML += myData[i].price / 100 + '€'
   }  
 }
