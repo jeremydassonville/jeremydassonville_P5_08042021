fb431d431631d6b45217c998fc5c81f51810f1db
Jeremy Dassonville <jeremydassonville07@gmail.com>
2021-04-13 17:46:35 +0200

    Ajout première version mis en page de la page produit

--- js/product.js
+++ js/product.js
@@ -28,13 +28,38 @@ function getCameraItem(myData, myId) {
 
 function displayMyCamera(myCamera) {
 
-    let displayProduct = document.getElementById('productName')
+    const displayProduct = document.getElementById('productDescription')
 
-    let productName = document.createElement('h3')
-
-    displayProduct.appendChild(productName)
+    /* Ajout de la structure HTML */
     
+    const productCard = document.createElement('div')
+    const productImage = document.createElement('img')
+    const productBody = document.createElement('div')
+    const productName = document.createElement('h3')
+    const productDescription = document.createElement('p')
+    const productPrice = document.createElement('p')
+
+    /* Ajout des éléments sur la page */
+
+    displayProduct.appendChild(productCard)
+    productCard.appendChild(productImage)
+    productCard.appendChild(productBody)
+    productBody.appendChild(productName)
+    productBody.appendChild(productDescription)
+    productBody.appendChild(productPrice)
+
+    /* Ajout des valeurs */
+
+    productCard.setAttribute("class", "card")
+    productImage.setAttribute("class", "card-img-top mx-auto mt-4")
+    productImage.setAttribute("src", myCamera.imageUrl)
+    productBody.setAttribute("class", "card-body")
+    productName.setAttribute("class","card-title")
     productName.innerHTML = myCamera.name
+    productDescription.setAttribute("class", "card-text")
+    productDescription.innerHTML = myCamera.description
+    productPrice.setAttribute("class", "cart-text")
+    productPrice.innerHTML = myCamera.price /100 + '€'
 }
 
 async function init () {
